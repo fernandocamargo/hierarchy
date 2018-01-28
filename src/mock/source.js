87259a7311c745e77f8dfa477d08bd149d706e8b
Fernando Camargo <camargodelbuono@gmail.com>
2018-01-28 18:08:20 +0100

    Mocking

---
+++ src/mock/source.js
@@ -0,0 +1,108 @@
+export default {
+  Jonas: {
+    position: 'CTO',
+    employees: [
+      {
+        Sophie: {
+          position: 'VP Engineering',
+          employees: [
+            {
+              Nick: {
+                position: 'Team Lead',
+                employees: [
+                  {
+                    Pete: {
+                      position: 'Backend Engineer',
+                      employees: [],
+                    },
+                  },
+                  {
+                    Barbara: {
+                      position: 'Fronted Engineer',
+                      employees: [],
+                    },
+                  },
+                  {
+                    Vanessa: {
+                      position: 'Fronted Engineer',
+                      employees: [],
+                    },
+                  },
+                ],
+              },
+            },
+            {
+              Phill: {
+                position: 'Team Lead',
+                employees: [
+                  {
+                    Amanda: {
+                      position: 'Backend Engineer',
+                      employees: [],
+                    },
+                  },
+                  {
+                    Christian: {
+                      position: 'Backend Engineer',
+                      employees: [
+                        {
+                          Ana: {
+                            position: 'Backend Engineer',
+                            employees: [
+                              {
+                                Katarina: {
+                                  position: 'Backend Engineer',
+                                  employees: [],
+                                },
+                              },
+                            ],
+                          },
+                        },
+                      ],
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      },
+    ],
+  },
+  Fernando: {
+    position: 'CEO',
+    employees: [
+      {
+        Derik: {
+          position: 'Backend Engineer',
+          employees: [
+            {
+              Jefferson: {
+                position: 'Backend Engineer',
+                employees: [],
+              },
+            },
+            {
+              Carina: {
+                position: 'Backend Engineer',
+                employees: [],
+              },
+            },
+          ],
+        },
+      },
+      {
+        Jasmina: {
+          position: 'Backend Engineer',
+          employees: [],
+        },
+      },
+      {
+        Natasha: {
+          position: 'Backend Engineer',
+          employees: [],
+        },
+      },
+    ],
+  },
+};
